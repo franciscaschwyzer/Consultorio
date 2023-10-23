bcb0140a9e84cc342f942334c208c40b5451f7ad
franciscaschwyzer <147095493+franciscaschwyzer@users.noreply.github.com>
2023-10-23 15:50:08 -0300

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace consultorio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
